5539c253ce0f05387ccd8d913ea7771cd7ee36e6
Luca Tabone <taboneluca3@gmail.com>
2017-03-18 10:02:48 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const path = require('path')
+const express = require('express')
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(8080, () => {
+  console.log('listening on port 8080')
+})
